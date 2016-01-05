2127323ce9b4f436d2ea1726c7973610864bbf96
KevinBacas <bacas.kevin@homtail.fr>
2016-01-05 20:34:56 +0000

    Added pin value getting

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template, jsonify
 from random import randint
+import RPi.GPIO as GPIO
+
+# Configuring GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.IN)
+
 
 app = Flask(__name__)
 
@@ -10,7 +16,8 @@ def index(name=None):
 @app.route('/value')
 def value():
     #TODO: inject GPIO value
-    return jsonify(value=randint(0,1000))
+    print 'value:', GPIO.input(12)
+    return jsonify(value=GPIO.input(12))
 
 if __name__ == "__main__":
   app.run('0.0.0.0')
